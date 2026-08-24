884ecafb7e62ab7c488483688fd677bfd06ebbe1
elpapi42 <whitman-2@hotmail.com>
2026-08-23 19:58:10 -0500

    chore: prepare CLI for SDK semantic projection

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -3,7 +3,7 @@ import { stripVTControlCharacters } from "node:util"
 import { Command, Option } from "commander"
 import { connectPiFleet, type AgentEvent } from "@elpapi42/pi-fleet-sdk"
 
-const version = "0.11.0"
+const version = "0.12.0"
 
 function splitPiArgs(args: string[]): { pifArgs: string[]; piArgs: string[] } {
   const separator = args.indexOf("--")
